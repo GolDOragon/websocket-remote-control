852057fc6c090fd06add71533363ae4be685157a
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2022-06-26 23:52:56 +0300

    feat: check connections

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,37 @@ const wss = new WebSocketServer({ port: WEBSOCKET_PORT });
 
 const sockets: WebSocket[] = [];
 
-wss.on('connection', (ws) => {
+type WsType = WebSocket & { isAlive?: boolean };
+
+wss.on('connection', (ws: WsType) => {
+  function heartbeat() {
+    ws.isAlive = true;
+  }
   sockets.push(ws);
   const wsStream = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
 
   wsStream.on('data', socketHandler(wsStream));
+  ws.on('pong', heartbeat);
+});
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws: WsType) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
+
+wss.on('close', () => {
+  clearInterval(interval);
 });
 
 process.on('SIGINT', () => {
   sockets.forEach((ws) => ws.close());
   process.exit();
+});
+
+process.on('exit', () => {
+  sockets.forEach((ws) => ws.close());
 });
